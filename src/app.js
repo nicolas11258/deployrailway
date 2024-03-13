60bd74b5634fe88d3045001adaa55139a795ac97
Nicolas <nicolas@gmail.com>
2024-03-13 18:39:49 -0500

    actualizar

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
+
 const PORT = process.env.PORT||8080;
 const connection = mongoose.connect(`mongodb+srv://nicolas11258:0kCg8T4wNf5Ne0zc@cluster0.sy13hmr.mongodb.net/clase_45?retryWrites=true&w=majority`)
 
